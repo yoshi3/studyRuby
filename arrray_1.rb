83aff9504e311c08f80d6eda462749ca021da601
katsuya.yoshizumi <katsuya.yoshizumi@linecorp.com>
2021-03-30 00:05:22 +0900

    :cat:

---
+++ arrray_1.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+cattle = "yahoo"
+p container = [cattle, cattle]
+cattle[2] = 'p'
+p cattle
---
+++ arrray_2.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/ruby
+a = 1
+b = "str"
+c = [a, b, 3, "文字列"]
+d = [a, c, [1, 2, 3]]
+p d
